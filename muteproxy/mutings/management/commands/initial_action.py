1e8ea4498943cacd254a54d3df013f978b18cbec
Emre Yilmaz <mail@emreyilmaz.me>
2018-10-04 13:22:46 +0300

    Remove unneeded blocks in the initial_action command

--- muteproxy/mutings/management/commands/initial_action.py
+++ muteproxy/mutings/management/commands/initial_action.py
@@ -1,14 +1,8 @@
-from django.core.management.base import BaseCommand, CommandError
-
-from mutings.models import Subscription, Token, Log
-from mutings.utils import get_sc_client
-import time
 import sys
+import time
 
-from steemconnect.operations import Mute
-from steemconnect.client import Client
-
-from django.core.signing import Signer
+from django.core.management.base import BaseCommand
+from mutings.models import Subscription
 
 
 class Command(BaseCommand):
@@ -16,17 +10,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         sys.stdout.write(f"Started..\n")
 
-        signer = Signer()
-
         while True:
             subscriptions = Subscription.objects.filter(
                     initial_action=False)
             for subscription in subscriptions:
-
-                token = Token.objects.get(user=subscription.from_user)
-                # @todo: check token expire time, and refresh it.
-                sc_client = Client(
-                    access_token=signer.unsign(token.access_token))
 
                 sys.stdout.write(f"Checking {subscription}\n")
 
